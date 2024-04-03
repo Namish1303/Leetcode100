765035b1f92b6316cfbd2ed1731a3818edc79095
Namish Arora <namish.arora@gmail.com>
2024-04-03 16:33:45 -0400

    validAnagram.py
    Solution for "Valid Anagram" Leetcode problem

---
+++ validAnagram.py
@@ -0,0 +1,24 @@
+'''
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+typically using all the original letters exactly once.
+
+'''
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s)==Counter(t)
+    
+'''
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if(len(s)!=len(t)):
+            return False
+        for i in t:
+            if s.count(i) != t.count(i):
+                return False
+    
+        return True
+'''
