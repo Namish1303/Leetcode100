7c55d509e858734480b89c14a2082da9cbd1d85c
Namish Arora <namish.arora@gmail.com>
2024-04-23 17:04:37 -0400

    addBinary.py
    Solution for "Add Binary" Leetcode problem

---
+++ addBinary.py
@@ -0,0 +1,9 @@
+'''
+Given two binary strings a and b, return their sum as a binary string.
+'''
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        c = int(a,2)+int(b,2)
+        c = format(c,'b')
+        return c
